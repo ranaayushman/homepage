bc09aca098c06e5f009d65d3d2ac9c068bba956d
Ayushman Rana <ayushmanr621@outlook.com>
2025-01-31 12:45:36 +0530

    need to add something

--- app/(main)/dashboard/ui/NewForm.tsx
+++ app/(main)/dashboard/ui/NewForm.tsx
@@ -1,14 +1,17 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const NewForm = () => {
   return (
     <div className="my-5 mx-10 ">
-      <Button className="bg-transparent text-[#3A471A] border border-[#3A471A] hover:bg-transparent w-40">
-        <Image height={20} width={20} src={"/svg/user.svg"} alt="user" />
-        New Form
-      </Button>
+      <Link href={"/registration"}>
+        <Button className="bg-transparent text-[#3A471A] border border-[#3A471A] hover:bg-transparent w-40">
+          <Image height={20} width={20} src={"/svg/user.svg"} alt="user" />
+          New Form
+        </Button>
+      </Link>
     </div>
   );
 };
