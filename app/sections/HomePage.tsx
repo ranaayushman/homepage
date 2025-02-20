3f0859fbdaea94a887951232d3e441c2e0dfd9dc
Ayushman Rana <ayushmanr621@outlook.com>
2025-02-21 02:15:40 +0530

    rounded image

--- app/sections/HomePage.tsx
+++ app/sections/HomePage.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import KalyaniForm from "./KalyaniForm";
+import Image from "next/image";
 
 const HomePage = () => {
   return (
-    <div>
-      <KalyaniForm />
+    <div className="grid grid-cols-2">
+      <div>
+        <KalyaniForm />
+      </div>
+      <div className="relative overflow-hidden">
+        <Image
+          src={"/img/students.png"}
+          width={1000}
+          height={1000}
+          alt={"kalyani"}
+          className="rounded-bl-[200px]"
+        />
+      </div>
     </div>
   );
 };
